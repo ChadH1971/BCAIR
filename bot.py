96378b84fa7b91f2dfbbba1a0e50f11c2f8176a7
Chad Hidalgo <hidalgochad@gmail.com>
2022-10-10 04:56:18 -0700

    changed .utils to .get which cleans up the code guild is predefined in the system environment variables and verifies connection at runtime by using the name= predicate

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
-my_guild = os.getenv("DISCORD_GUILD")
+guild = os.getenv("DISCORD_GUILD")
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -14,13 +14,9 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == my_guild:
-            break
+    GUILD = discord.utils.get(client.guilds, name=guild)
 
-    print(f'{client.user} has connected to {guild.name} (id: {guild.id})')
+    print(f'{client.user} is connected to {GUILD.name} (id: {GUILD.id})')
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
 
 client.run(token)
